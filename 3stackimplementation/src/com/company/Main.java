0dc6c6635ff2c6e884eaae1a66d8098a44a91770
Jongrin Kim <jongrinkim89@gmail.com>
2016-09-24 18:06:28 -0700

    Stack implementation practice

---
+++ 3stackimplementation/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    //Describe how you could use a single array to implement three stacks
+
+    //Step 1: How to simulate single stack using single array?
+
+
+}
